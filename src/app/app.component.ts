1068cae84aa324b12242f2ef78b4c8e3b5b77d16
Josué Hoenicka <josuee.vzla@gmail.com>
2023-01-12 11:33:11 -0300

    update: i learned property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,16 @@ export class AppComponent {
   title = 'ecommerce-plataform';
   favNumber:number = 3;
   img: string = 'https://source.unsplash.com/random'
+  btnDisabled: boolean = true;
+  progress_a: number = 100;
+  progress_b: number = 58;
+  person = {
+    name: 'Josue',
+    age: 20,
+    heLovesAngular: true
+  };
+
+
   constructor () {
   }
 }
