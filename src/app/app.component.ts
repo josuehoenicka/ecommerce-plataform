c1e25ae787184b151b1eb14cda5e62f5bf35d125
Josué Hoenicka <josuee.vzla@gmail.com>
2023-01-12 08:25:02 -0300

    update: i practiced interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'ecommerce-plataform';
+  favNumber:number = 3;
+  img: string = 'https://source.unsplash.com/random'
+  constructor () {
+  }
 }
