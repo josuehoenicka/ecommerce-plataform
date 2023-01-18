5d3714ff5a9885973e20bb3ad8520fba93b2f576
Josué Hoenicka <josuee.vzla@gmail.com>
2023-01-18 13:47:49 -0300

    new: add import angular http / add get api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+// import @angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+// import app
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+// import components
 import { ImgComponent } from './components/img/img.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
@@ -21,6 +25,7 @@ import { NavComponent } from './components/nav/nav.component';
     imports: [
         BrowserModule,
         FormsModule,
+        HttpClientModule,
         AppRoutingModule
     ]
 })
