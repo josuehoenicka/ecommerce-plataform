eff57474c263535b8e7bcc95bf096d7ff431a82a
Josué Hoenicka <josuee.vzla@gmail.com>
2023-01-23 12:42:37 -0300

    new: add swiper scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
-// swiper
-import { SwiperModule } from 'swiper/angular';
 // app
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+// swiper
+import { SwiperModule } from 'swiper/angular';
 // components
 import { ImgComponent } from './components/img/img.component';
 import { ProductComponent } from './components/product/product.component';
