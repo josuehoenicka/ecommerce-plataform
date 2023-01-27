dbaae257bae19fdd688fb7f99152dae74746d269
Josué Hoenicka <josuee.vzla@gmail.com>
2023-01-27 12:22:10 -0300

    update: config auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,26 @@
+// @angular
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// environment
+import { environment } from './../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = `${environment.API_URL}/api/auth`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login(email: string, password: string) {
+    return this.http.post(`${this.apiUrl}/login`, {email, password});
+  }
+
+  profile() {
+    return this.http.get(`${this.apiUrl}/profile`);
+  }
+
 }
