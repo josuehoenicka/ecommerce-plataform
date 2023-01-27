8ca6706b93a7a15607eb3d632cfc3cdae5b6fd33
Josué Hoenicka <josuee.vzla@gmail.com>
2023-01-27 09:19:35 -0300

    update: transfer file

---
+++ src/app/test/product.ts
@@ -0,0 +1,32 @@
+const myPersonalProducts = [
+  {
+    id: 1,
+    title: 'Army Of Two II',
+    price: 45,
+    image: '../../../assets/images/aot2.jpg'
+  },
+  {
+    id: 2,
+    title: 'inFAMOUS 2',
+    price: 40,
+    image: '../../../assets/images/infamous2.jpg'
+  },
+  {
+    id: 3,
+    title: 'All-Stars',
+    price: 40,
+    image: '../../../assets/images/allstar.jpg'
+  },
+  {
+    id: 4,
+    title: 'Minecraft',
+    price: 35,
+    image: '../../../assets/images/minecraft.jpg'
+  },
+  {
+    id: 5,
+    title: 'Ratchet',
+    price: 25,
+    image: '../../../assets/images/ratchet.jpg'
+  }
+]
