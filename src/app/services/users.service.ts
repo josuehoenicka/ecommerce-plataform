b98855c089efb7a80ad031d24769483149c5b457
Josué Hoenicka <josuee.vzla@gmail.com>
2023-01-27 12:14:27 -0300

    update: config user auth

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,29 @@
+// @angular
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// models
+import { User, CreateUserDTO } from './../models/user.model';
+// environments
+import { environment } from './../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  private apiUrl = `${environment.API_URL}/api/users`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  create(dto: CreateUserDTO) {
+    return this.http.post<User>(this.apiUrl, dto);
+  }
+
+  getAll(dto: CreateUserDTO) {
+    return this.http.get<User[]>(this.apiUrl);
+  }
+
 }
